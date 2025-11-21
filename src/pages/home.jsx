e54ce8daf47ba9efc6296c1aae5d982cbccfbbcc
MD. Imteaj Rana <imteaj.rana2@gmail.com>
2025-11-22 01:53:02 +0600

    4th

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Slider from "../components/Slider";
+
+const Home = () => {
+    return(
+        <div>
+            <title>Home</title>
+            <Slider></Slider>
+        </div>
+    );
+};
+
+export default Home;
